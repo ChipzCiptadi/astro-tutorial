a973e9e5713fe3513bc19886cde77678ef32b78f
Ciptadi <chipz.ciptadi@gmail.com>
2025-07-24 06:30:32 +0700

    added tag index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        allowedHosts: ["devserver-main--astro-learn-123123.netlify.app"]
-    }
+  site: "http://devserver-main--astro-learn-123123.netlify.app",
+  server: {
+    allowedHosts: ["devserver-main--astro-learn-123123.netlify.app"],
+  },
 });
